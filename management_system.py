970c03c60dd5131ce1ba0af8f1b1817813227730
yeshwanthkotha <65798301+yeshwanthkotha@users.noreply.github.com>
2024-10-16 23:35:35 -0400

    update-extract_customers

--- management_system.py
+++ management_system.py
@@ -5,5 +5,18 @@ def load_orders(file):
         orders = json.load(f)
     return orders
 
+def extract_customers(orders):
+    customers = {}
+    for order in orders:
+        phone = order["phone"]
+        name = order["name"]
+        # Avoid overwriting if the phone number already exists
+        if phone not in customers:
+            customers[phone] = name
+    return customers
+
+
 orders = load_orders('example_orders.json')
-print(orders)
+#print(orders)
+customers = extract_customers(orders)
+print(customers)
